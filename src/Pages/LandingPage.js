3069b78663e3139028719e57eba3374a02c14e65
Chuks Festus <cutogu@gmail.com>
2018-01-05 18:58:25 +0100

    optional display for homepage

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import PropTypes from 'prop-types'
 
-const LandingPage = () => (
-  <div>
-    <h1>hello</h1>
-    <Link to='/login'>Login</Link>
-  </div>
+import Home from './Home/Home'
+
+const LandingPage = ({ isAuthenticated }) => (
+  <React.Fragment>
+    { isAuthenticated ? 
+      <Home /> : 
+      <div>
+        <h1>hello</h1>
+        <Link to='/login'>Login</Link>
+      </div> 
+    }
+  </React.Fragment>
 )
 
-export default LandingPage
+LandingPage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+}
+
+function mapStateToProps(state) {
+  return{
+    isAuthenticated: !!state.user.token
+  }
+}
+
+export default connect(mapStateToProps)(LandingPage)

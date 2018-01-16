59dcb377e65304cdfc72d14eced704d4b2ca0bd2
Promise Tochi <Promhize@gmail.com>
2018-01-16 22:16:09 +0100

    Detached signup functionality from redux

--- src/api.js
+++ src/api.js
@@ -1,15 +1,19 @@
-import axios from "axios";
+import axios from 'axios'
 
-const BASEURL = "ttps://obscure-waters-44612.herokuapp.com/";
+const BASEURL = 'ttps://obscure-waters-44612.herokuapp.com/'
 
 export default {
   user: {
-    login: credentails => 
-      axios.post(`${BASEURL}api/v1/auth/user`, credentails).then(res => res.data.user),
-    signup: user => 
-      axios.post(`${BASEURL}api/v1/user`, user, { headers: { 'Content-Type': 'application/form-data', 'Accept': 'application/form-data' } }, ).then(res => res.data.user),
+    login: credentails => axios.post(`${BASEURL}api/v1/auth/user`, credentails),
+    signup: user =>
+      axios.post(`${BASEURL}api/v1/user`, user, {
+        headers: {
+          'Content-Type': 'application/form-data',
+          Accept: 'application/form-data',
+        },
+      }),
     resetPasswordRequest: email =>
-      axios.post(`${BASEURL}api/vi/user/reset`, email)
+      axios.post(`${BASEURL}api/vi/user/reset`, email),
   },
   // posts: {
   //   fetchAll: () => axios.get(`${BASEURL}api/v1/social/post/`).then(res => res.data.post),

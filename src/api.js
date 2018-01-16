3909931c311a476885de7a19d597662208718c01
Mary Mazi <mazi.mary.o@gmail.com>
2018-01-16 21:48:00 +0100

    Fine-tune login functionality

--- src/api.js
+++ src/api.js
@@ -5,7 +5,9 @@ const BASEURL = "ttps://obscure-waters-44612.herokuapp.com/";
 export default {
   user: {
     login: credentails => 
-      axios.post(`${BASEURL}api/v1/auth/user`, credentails).then(res => res.data.user),
+      axios.post(`${BASEURL}api/v1/auth/user`, credentails).then(
+        res => res.data
+      ),
     signup: user => 
       axios.post(`${BASEURL}api/v1/user`, user, { headers: { 'Content-Type': 'application/form-data', 'Accept': 'application/form-data' } }, ).then(res => res.data.user),
     resetPasswordRequest: email =>

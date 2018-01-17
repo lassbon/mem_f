3468002c56fc0a6dc85ec0f2fb5c3849d647c111
Chuks Festus <cutogu@gmail.com>
2018-01-17 09:52:17 +0100

    advert section fixed

--- src/components/Advert/Advert.js
+++ src/components/Advert/Advert.js
@@ -4,7 +4,7 @@ import { Advertisement } from 'semantic-ui-react';
 import './Advert.css';
 
 const Advert = () => (
-  <div>
+  <div style={{marginLeft: '75vw'}}>
     <Advertisement className="add_name" unit='medium rectangle' test='Medium Rectangle' />
     <Advertisement className="add_name2" unit='medium rectangle' test='Medium Rectangle' />
   </div>

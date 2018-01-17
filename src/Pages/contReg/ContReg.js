4a66b755e7e88022683fa46191ba673084d60929
chiefoleka <valentinenwakibeya@gmail.com>
2018-01-17 20:52:40 +0100

    Made all the data come to the state so just run validation if you have time, then send to api

--- src/Pages/contReg/ContReg.js
+++ src/Pages/contReg/ContReg.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, {Component} from "react";
 import {
   Container,
   Grid,
   Image,
   Segment,
   Form,
-  Checkbox,
   Button
 } from "semantic-ui-react";
 
@@ -25,100 +24,150 @@ function getSteps() {
   ];
 }
 
-const employies = [
-  "1 - 10",
-  "11 - 20",
-  "21 - 40",
-  "41 - 80",
-  "81 - 100",
-  "100 and above"
-];
+class ContReg extends Component {
+  state = {
+    aReturn : null,
+    employees : null,
+    profits : null,
+    companyName : null,
+    companyAddress : null,
+    companyBusiness : null,
+    companyPhone : null,
+    loading : false
+  }
 
-const returns = [
-  "N100,000 - N500,000",
-  "N501,000 - N1,000,000",
-  "N1,000,001 - N3,000,000",
-  "N3,000,001 - N5,000,000",
-  "N5,000,001 - N10,000,000",
-  "N100,000,001 and above"
-];
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value})
+  }
 
-const NumOfEmployees = employies.map(num => (
-  <div key={`${num} + ${num}`}>
-    <Checkbox style={{ marginBottom: 20 }} radio label={num} />
-    <br />
-  </div>
-));
+  onSubmit = () => {
+    console.log(this.state)
+    this.setState({ loading: true })
+    setTimeout(function() { window.location = "/cont2" }, 3000)
+  }
 
-const AnualReturns = returns.map(aReturn => (
-  <div key={`${aReturn} + ${aReturn}`}>
-    <Checkbox style={{ marginBottom: 20 }} radio label={aReturn} />
-    <br />
-  </div>
-));
+  validate = () => {
+    // perform validation here
+  }
 
-const AnualProfits = returns.map(aProfit => (
-  <div key={`${aProfit} + ${aProfit}`}>
-    <Checkbox style={{ marginBottom: 20 }} radio label={aProfit} />
-    <br />
-  </div>
-));
+  render(){
+    const employies = [
+      "1 - 10",
+      "11 - 20",
+      "21 - 40",
+      "41 - 80",
+      "81 - 100",
+      "100 and above"
+    ];
 
-const ContReg = () => (
-  <div
-    style={{
-      width: "70%",
-      margin: "0 auto",
-      border: "1px solid #C0C0C0",
-      height: "100%",
-      verticalAlign: "middle",
-      marginTop: "100px",
-      marginBottom: 100
-    }}
-  >
-    <Image
-      style={{ marginBottom: 20, marginLeft: "36%", marginTop: 30 }}
-      verticalAlign="middle"
-      src="http://www.accinigeria.com/wp-content/uploads/2017/10/ACCIHD2-2.png"
-    />
-    <Form style={{ marginTop: 30 }}>
-      <Form.Field style={{ width: "55%", margin: "10px auto" }}>
-        <input placeholder="Company's Name" />
-      </Form.Field>
-      <Form.Field style={{ width: "55%", margin: "10px auto" }}>
-        <input placeholder="Address" />
-      </Form.Field>
-      <Form.Field style={{ width: "55%", margin: "10px auto" }}>
-        <input placeholder="Phone Number" />
-      </Form.Field>
-      <Form.Field
-        style={{ width: "55%", margin: "10px auto", marginBottom: 50 }}
+    const returns = [
+      "N100,000 - N500,000",
+      "N501,000 - N1,000,000",
+      "N1,000,001 - N3,000,000",
+      "N3,000,001 - N5,000,000",
+      "N5,000,001 - N10,000,000",
+      "N100,000,001 and above"
+    ];
+
+    const NumOfEmployees = employies.map(num => (
+      <div key={`${num} + ${num}`}>
+        <Form.Field 
+          label={num} 
+          control='input' 
+          type='radio' 
+          name="employees"
+          value={num} 
+          checked={this.state.employees === `${num}`}
+          onChange={this.handleChange}
+        />
+        <br />
+      </div>
+    ));
+
+    const AnualReturns = returns.map(aReturn => (
+      <div key={`${aReturn} + ${aReturn}`}>
+        <Form.Field 
+          label={aReturn} 
+          control='input' 
+          type='radio' 
+          name="aReturn"
+          value={aReturn}
+          checked={this.state.aReturn === `${aReturn}`}
+          onChange={this.handleChange} 
+        />
+        <br />
+      </div>
+    ));
+
+    const AnualProfits = returns.map(aProfit => (
+      <div key={`${aProfit} + ${aProfit}`}>
+        <Form.Field 
+          label={aProfit} 
+          control='input' 
+          type='radio' 
+          value={aProfit}
+          name="profits"
+          checked={this.state.profits === `${aProfit}`}
+          onChange={this.handleChange}  
+          />
+        <br />
+      </div>
+    ));
+    return(
+      <div
+        style={{
+          width: "70%",
+          margin: "0 auto",
+          border: "1px solid #C0C0C0",
+          height: "100%",
+          verticalAlign: "middle",
+          marginTop: "100px",
+          marginBottom: 100
+        }}
       >
-        <input placeholder="Nature of buisness" />
-      </Form.Field>
-      <Grid style={{ width: "90%", margin: "0 auto" }}>
-        <Grid.Column width={5}>
-          <h3 style={{ color: "#656768" }}>Number of Employees</h3>
-          {NumOfEmployees}
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <h3 style={{ color: "#656768" }}>Annual Return</h3>
-          {AnualReturns}
-        </Grid.Column>
-        <Grid.Column width={5}>
-          <h3 style={{ color: "#656768" }}>Annual Profits</h3>
-          {AnualProfits}
-        </Grid.Column>
-      </Grid>
-      <Button
-        as={Link}
-        to="/cont2"
-        className="btn btn-reverse"
-        content="Next"
-        style={{marginLeft: '40%'}}
-      />
-    </Form>
-  </div>
-);
+        <Image
+          style={{ marginBottom: 20, marginLeft: "36%", marginTop: 30 }}
+          verticalAlign="middle"
+          src="http://www.accinigeria.com/wp-content/uploads/2017/10/ACCIHD2-2.png"
+        />
+        <Form style={{ marginTop: 30 }} onSubmit={this.onSubmit} loading={this.state.loading}>
+          <Form.Field style={{ width: "55%", margin: "10px auto" }}>
+            <input placeholder="Company's Name" name="companyName" onChange={this.handleChange}  />
+          </Form.Field>
+          <Form.Field style={{ width: "55%", margin: "10px auto" }}>
+            <input placeholder="Address" name="companyAddress" onChange={this.handleChange}/>
+          </Form.Field>
+          <Form.Field style={{ width: "55%", margin: "10px auto" }}>
+            <input placeholder="Phone Number" name="companyPhone" onChange={this.handleChange}/>
+          </Form.Field>
+          <Form.Field
+            style={{ width: "55%", margin: "10px auto", marginBottom: 50 }}
+          >
+            <input placeholder="Nature of buisness" name="companyBusiness" onChange={this.handleChange}/>
+          </Form.Field>
+          <Grid style={{ width: "90%", margin: "0 auto" }}>
+            <Grid.Column width={5}>
+              <h3 style={{ color: "#656768" }}>Number of Employees</h3>
+              {NumOfEmployees}
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <h3 style={{ color: "#656768" }}>Annual Return</h3>
+              {AnualReturns}
+            </Grid.Column>
+            <Grid.Column width={5}>
+              <h3 style={{ color: "#656768" }}>Annual Profits</h3>
+              {AnualProfits}
+            </Grid.Column>
+          </Grid>
+          <Button
+            className="btn btn-reverse"
+            content="Next"
+            style={{marginLeft: '40%'}}
+          />
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default ContReg
--- src/Pages/contReg/ContReg3.js
+++ src/Pages/contReg/ContReg3.js
@@ -2,87 +2,125 @@ import React from 'react';
 import { Grid, Segment, Form, Image, Button } from "semantic-ui-react";
 import { Link } from 'react-router-dom';
 
-const ContReg3 = () => (
-  <React.Fragment>
-    <Form style={{ width: "70%", margin: "0 auto", textAlign: "center" }}>
-      <Image
-        style={{ marginBottom: 20, marginTop: 30 }}
-        verticalAlign="middle"
-        src="http://www.accinigeria.com/wp-content/uploads/2017/10/ACCIHD2-2.png"
-      />
-      <h2>Company Representatives</h2>
-      <Grid columns="equal">
-        <Grid.Column>
-          <Form.Field>
-            <Form.Input placeholder="Name" />
-          </Form.Field>
-          <Form.Field>
-            <Form.Input type="email" placeholder="email" />
-          </Form.Field>
-          <Form.Field>
-            <Form.Input placeholder="Phone number" />
-          </Form.Field>
-          <Form.Field>
-            <label>
-              Upload copy of Certificate of Incorporation or Registration of
-              Applicant
-            </label>
-            <Form.Input type="file" />
-          </Form.Field>
-          <Form.Field>
-            <label>
-              Upload Passport Photographs of the Company Representative(s)
-            </label>
-            <Form.Input type="file" />
-          </Form.Field>
-          <Form.Field>
-            <label>Upload curriculum Vitae of Company Representative(s)</label>
-            <Form.Input type="file" />
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column>
+class ContReg3 extends React.Component {
+  state = {
+    loading : false,
+    rep1 : {
+      name : null,
+      email : null,
+      phone : null,
+      regOfApp : null,
+      picture : null,
+      cv : null
+    },
+    rep2 : {
+      name : null,
+      email : null,
+      phone : null,
+      regOfApp : null,
+      picture : null,
+      cv : null
+    }
+  }
+
+  handleChange1 = (e) => {
+    this.setState({ 
+    rep1 : {...this.state.rep1, [e.target.name]: e.target.value}})
+  }
+
+  handleChange2 = (e) => {
+    this.setState({ 
+    rep2 : {...this.state.rep1, [e.target.name]: e.target.value}})
+  }
+
+  submit = () => {
+    console.log(this.state)
+    this.setState({ loading: true })
+    setTimeout(function() { window.location = "/cont4" }, 3000)
+  }
+
+  validate = () => {
+    // perform validation here
+  }
+
+  render(){
+    return(
+      <Form style={{ width: "70%", margin: "0 auto", textAlign: "center" }}>
+        <Image
+          style={{ marginBottom: 20, marginTop: 30 }}
+          verticalAlign="middle"
+          src="http://www.accinigeria.com/wp-content/uploads/2017/10/ACCIHD2-2.png"
+        />
+        <h2>Company Representatives</h2>
+        <Grid columns="equal">
           <Grid.Column>
-            <Form.Field>
-              <Form.Input placeholder="Name" />
+            <Form.Field loading={this.state.loading}>
+              <Form.Input placeholder="Name" name="name" onChange={this.handleChange1}/>
             </Form.Field>
             <Form.Field>
-              <Form.Input type="email" placeholder="email" />
+              <Form.Input type="email" placeholder="email" name="email" onChange={this.handleChange1}/>
             </Form.Field>
             <Form.Field>
-              <Form.Input placeholder="Phone number" />
+              <Form.Input placeholder="Phone number" name="phone" onChange={this.handleChange1}/>
             </Form.Field>
             <Form.Field>
               <label>
                 Upload copy of Certificate of Incorporation or Registration of
                 Applicant
               </label>
-              <Form.Input type="file" />
+              <Form.Input type="file" name="regOfApp" onChange={this.handleChange1}/>
             </Form.Field>
             <Form.Field>
               <label>
                 Upload Passport Photographs of the Company Representative(s)
               </label>
-              <Form.Input type="file" />
+              <Form.Input type="file" name="picture" onChange={this.handleChange1}/>
+            </Form.Field>
+            <Form.Field>
+              <label>Upload curriculum Vitae of Company Representative(s)</label>
+              <Form.Input type="file" name="cv" onChange={this.handleChange2}/>
+            </Form.Field>
+          </Grid.Column>
+          <Grid.Column>
+            <Form.Field loading={this.state.loading}>
+              <Form.Input placeholder="Name" name="name" onChange={this.handleChange2}/>
+            </Form.Field>
+            <Form.Field>
+              <Form.Input type="email" placeholder="email" name="email" onChange={this.handleChange2}/>
+            </Form.Field>
+            <Form.Field>
+              <Form.Input placeholder="Phone number" name="phone" onChange={this.handleChange2}/>
             </Form.Field>
             <Form.Field>
               <label>
-                Upload curriculum Vitae of Company Representative(s)
+                Upload copy of Certificate of Incorporation or Registration of
+                Applicant
               </label>
-              <Form.Input type="file" />
+              <Form.Input type="file" name="regOfApp" onChange={this.handleChange2}/>
+            </Form.Field>
+            <Form.Field>
+              <label>
+                Upload Passport Photographs of the Company Representative(s)
+              </label>
+              <Form.Input type="file" name="picture" onChange={this.handleChange2}/>
+            </Form.Field>
+            <Form.Field>
+              <label>Upload curriculum Vitae of Company Representative(s)</label>
+              <Form.Input type="file" name="cv" onChange={this.handleChange2}/>
             </Form.Field>
           </Grid.Column>
-        </Grid.Column>
-      </Grid>
-      <div style={{marginTop: 30}}>
-        <Button to="/cont2" as={Link}>
-          Back
-        </Button>
-        <Button className='btn' to="/cont4" as={Link}>
-          Next
-        </Button>
-      </div>
-    </Form>
-  </React.Fragment>
-);
+        </Grid>
+        <div style={{marginTop: 30}}>
+          <Button to="/cont2" as={Link}>
+            Back
+          </Button>
+          <Button className='btn' onClick={this.submit}>
+            Next
+          </Button>
+        </div>
+      </Form>
+    )
+  }
+}
 
 export default ContReg3

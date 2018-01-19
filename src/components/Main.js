4e4db51cd5c701f5369f83ef99b642465f1b21b6
Mary Mazi <mazi.mary.o@gmail.com>
2018-01-19 16:48:21 +0100

    - Convert Sidebar to a class component

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,8 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import TopNav from './TopNav/TopNav'
 import SideBar from './SideBar/SideBar'
 import Advert from './Advert/Advert'
-
 import Home from '../Pages/Home/Home'
-// import Profile from '../Pages/Profile'
+import Profile from '../Pages/Profile'
 import Profile2 from '../Pages/Profile2'
 import Project from '../Pages/Project'
 import Library from '../Pages/Library'
@@ -16,18 +15,18 @@ import Payment from '../Pages/Payment'
 const Main = ({ match }) => (
   <React.Fragment>
     <TopNav />
-    <SideBar />
+    <SideBar match={match} />
     <Advert />
     <main className="main">
       {console.log(match)}
       <Switch>
         <Route path={`${match.path}`} exact component={Home} />
-        {/* <Route exact path={`${match.path}/profile`} render={Profile} /> */}
-        {/* <Route path={`${match.path}/profile:friend`} component={Profile2} /> */}
+        <Route exact path={`${match.path}/profile`} render={Profile} />
+        <Route path={`${match.path}/profile:friend`} component={Profile2} />
         <Route exact path={`${match.path}/project`} component={Project} />
         <Route exact path={`${match.path}/library`} component={Library} />
         <Route exact path={`${match.path}/discuss`} component={Discussions} />
-        {/* <Route exact path={`${match.path}/payment`} component={Payment} /> */}
+        <Route exact path={`${match.path}/payment`} component={Payment} />
         <Redirect to={`${match.url}`} />
       </Switch>
     </main>

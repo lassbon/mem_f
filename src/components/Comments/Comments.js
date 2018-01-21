ddbc789503246940571430b65a47a336d338ce31
chiefoleka <valentinenwakibeya@gmail.com>
2018-01-21 15:51:09 +0100

    Fixed how comments are displayed, then prepared the comments component for sending comments. Comments are not still sent yet though

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,13 +9,13 @@ const BASEURL = 'https://obscure-waters-44612.herokuapp.com/'
 class Comments extends Component {
   constructor() {
     super()
-    this.state = { comments: [], visible: false  }
+    this.state = { comments: [], visible: false, loading : false  }
   }
 
   state_setComments = comments => {
     this.setState(state => ({ ...state, comments }))
   }
-  getComments() {
+  getComments = () => {
     const { user: { token, id } } = this.props
     // console.log(this.props)
     // console.log(token, id)
@@ -27,6 +27,24 @@ class Comments extends Component {
     }).then(response => {
       // console.log('response', response)
       this.state_setComments(response.data)
+    })
+  }
+  replyComment = event => {
+    const { user: { token, id } } = this.props
+    let data = event.target.querySelector('textarea').value
+    this.setState({
+      loading : !this.state.loading
+    })
+    axios.post(`${BASEURL}api/v1/social/comment`, {data : data}, {
+      headers: {
+        authorization: token,
+      },
+    }).then(response => {
+      // console.log('response', response)
+      this.state_setComments(response.data)
+      this.setState({
+        loading : !this.state.loading
+      })
     })
   }
 
@@ -51,7 +69,7 @@ class Comments extends Component {
             <Icon name='comment' />
             comment
           </Button>
-          <Label as='a' basic color='blue' pointing='left'>2,048</Label>
+          <Label as='a' basic color='blue' pointing='left'>{comments.length}</Label>
         </Button>
         <Divider hidden />
         <Transition visible={visible} animation='slide down' duration={500}>
@@ -67,20 +85,15 @@ class Comments extends Component {
                       <Comment.Text>{com.comment}</Comment.Text>
                     </Comment.Content>
                   </Comment>
-                  <Form reply>
-                    <Form.TextArea autoHeight rows={2} placeholder='leave a comment...' />
-                    <Button content='Add Reply' size='tiny' labelPosition='left' icon='edit' primary />
-                  </Form>
                   </React.Fragment>
                 
               ))
               .reverse()
-          ) : (
-                <Form reply>
-                  <Form.TextArea autoHeight rows={2} placeholder='leave a comment...' />
-                  <Button content='Add Reply' size='tiny' labelPosition='left' icon='edit' primary />
-                </Form>
-          )}
+          ) : false}
+          <Form reply onSubmit={this.replyComment} loading={this.state.loading}>
+            <Form.TextArea autoHeight rows={2} placeholder='leave a comment...' />
+            <Button content='Add Comment' size='tiny' labelPosition='left' icon='edit' primary />
+          </Form>
           </Comment.Group>
         </Transition>
       </React.Fragment>

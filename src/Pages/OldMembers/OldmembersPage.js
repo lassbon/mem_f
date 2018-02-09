d66ba5a51b2689749828d705da66a7ed8ca16bb4
Chuks Festus <cutogu@gmail.com>
2018-02-09 15:57:31 +0100

    update

--- src/Pages/OldMembers/OldmembersPage.js
+++ src/Pages/OldMembers/OldmembersPage.js
@@ -1,36 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { oldMem } from '../../actions/auth'
-import { Message, Grid, Image } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { oldMem } from "../../actions/auth";
+import { Message, Grid, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-import OldmembersForm from './OldmembersForm'
-
-const registrationStages = [
-  '/old',
-  '/old2',
-]
+import OldmembersForm from "./OldmembersForm";
 
 class OldmembersPage extends React.Component {
-  submit = data =>
-    this.props.oldMem(data)
-    // .then(user => {
-    //   const { history } = this.props
-    //   const { regState } = user
-    //   console.log(regState, registrationStages[regState])
-    //   regState > 2
-    //     ? history.push('/app')
-    //     : history.push(registrationStages[regState])
-    //   // this.props.history.push('/app')
-    // })
+  submit = data => this.props.oldMem(data);
+  // .then(user => {
+  //   const { history } = this.props
+  //   const { regState } = user
+  //   console.log(regState, registrationStages[regState])
+  //   regState > 2
+  //     ? history.push('/app')
+  //     : history.push(registrationStages[regState])
+  //   // this.props.history.push('/app')
+  // })
 
   render() {
     return (
       <div className="login-form">
         <Grid
           textAlign="center"
-          style={{ height: '100%' }}
+          style={{ height: "100%" }}
           verticalAlign="middle"
         >
           <Grid.Column style={{ maxWidth: 450 }}>
@@ -41,27 +35,27 @@ class OldmembersPage extends React.Component {
             />
             <OldmembersForm submit={this.submit} />
             <Message>
-              New to us?{' '}
-              <Link to="/signup" style={{ color: 'var(--main-blue)' }}>
+              New to us?{" "}
+              <Link to="/signup" style={{ color: "var(--main-blue)" }}>
                 Sign Up
               </Link>
             </Message>
 
-            <Link to="/forgotpassword" style={{ color: 'var(--main-blue)' }}>
+            <Link to="/forgotpassword" style={{ color: "var(--main-blue)" }}>
               forgot password?
             </Link>
           </Grid.Column>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
 OldmembersPage.propTypes = {
   history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired
   }).isRequired,
-  oldMem: PropTypes.func.isRequired,
-}
+  oldMem: PropTypes.func.isRequired
+};
 
-export default connect(null, { oldMem })(OldmembersPage)
+export default connect(null, { oldMem })(OldmembersPage);

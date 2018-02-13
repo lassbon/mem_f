0bde0c4b9052202f99b4c670d4d305c93ca7d1be
Promise Tochi <Promhize@gmail.com>
2018-02-13 20:34:30 +0100

    reg updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
-
+import { connect } from 'react-redux'
 import LandingPage from './Pages/LandingPage'
 import LoginPage from './Pages/LoginPage/LoginPage'
 // import Home from './Pages/Home/Home'
@@ -30,53 +30,63 @@ import Reject from './components/messages/Reject'
 import ContReg7 from './Pages/contReg/ContReg7'
 import RestPassword from './Pages/ResetPassword'
 
-const App = ({ location }) => (
-  <div>
-    <Switch>
-      <Route location={location} path="/" exact component={LandingPage} />
+const App = ({ location, user }) => {
+  if (!user.id && location.pathname !== '/login') {
+    return <Redirect to="/login" />
+  }
+  return (
+    <div>
+      <Switch>
+        <Route location={location} path="/" exact component={LandingPage} />
 
-      <RegRoute exact path="/cont" component={ContReg} />
-      <RegRoute exact path="/cont2" component={ContReg2} />
-      <GuestRoute exact path="/old" component={OldMembers} />
-      <GuestRoute exact path="/old2" component={OldMembers2} />
-      <RegRoute exact path="/cont3/" component={ContReg3} />
-      <RegRoute exact path="/cont4" component={ContReg4} />
-      <RegRoute exact path="/cont4/:status" component={ContReg4} />
-      <RegRoute exact path="/cont5" component={ContReg5} />
-      <RegRoute exact path="/cont6" component={ContReg7} />
-      <RegRoute exact path="/cont7" component={ContReg6} />
-      <RegRoute exact path="/confirmation" component={Confirmation} />
-      <Route exact path="/regmessage" component={RegMessage} />
-      <Route exact path="/payment" component={Payment} />
-      <Route exact path="/profile" component={Profile} />
-      <Route exact path="/events" component={EventPage} />
-      <Route exact path="/confirm/:id/:user" component={Confirm} />
-      <Route exact path="/reject/:id/:refereeId" component={Reject} />
-      {/* <Route location={location} path='/login' exact component={LoginPage} /> */}
-      <Route location={location} path="/signup" exact component={SignUpPage} />
-      <Route
-        location={location}
-        path="/forgotpassword"
-        exact
-        component={ForgotPassword}
-      />
-      <Route
-        location={location}
-        path="/resetpassword"
-        exact
-        component={RestPassword}
-      />
-      <Route location={location} path="/login" exact component={LoginPage} />
-      <Route
-        location={location}
-        path="/onboard/:id"
-        exact
-        component={OldmembersPage}
-      />
-      <UserRoute location={location} path="/app" component={Main} />
-    </Switch>
-  </div>
-)
+        <RegRoute exact path="/cont" component={ContReg} />
+        <RegRoute exact path="/cont2" component={ContReg2} />
+        <GuestRoute exact path="/old" component={OldMembers} />
+        <GuestRoute exact path="/old2" component={OldMembers2} />
+        <RegRoute exact path="/cont3/" component={ContReg3} />
+        <RegRoute exact path="/cont4" component={ContReg4} />
+        <RegRoute exact path="/cont4/:status" component={ContReg4} />
+        <RegRoute exact path="/cont5" component={ContReg5} />
+        <RegRoute exact path="/cont6" component={ContReg7} />
+        <RegRoute exact path="/cont7" component={ContReg6} />
+        <RegRoute exact path="/confirmation" component={Confirmation} />
+        <Route exact path="/regmessage" component={RegMessage} />
+        <Route exact path="/payment" component={Payment} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/events" component={EventPage} />
+        <Route exact path="/confirm/:id/:user" component={Confirm} />
+        <Route exact path="/reject/:id/:refereeId" component={Reject} />
+        {/* <Route location={location} path='/login' exact component={LoginPage} /> */}
+        <Route
+          location={location}
+          path="/signup"
+          exact
+          component={SignUpPage}
+        />
+        <Route
+          location={location}
+          path="/forgotpassword"
+          exact
+          component={ForgotPassword}
+        />
+        <Route
+          location={location}
+          path="/resetpassword"
+          exact
+          component={RestPassword}
+        />
+        <Route location={location} path="/login" exact component={LoginPage} />
+        <Route
+          location={location}
+          path="/onboard/:id"
+          exact
+          component={OldmembersPage}
+        />
+        <UserRoute location={location} path="/app" component={Main} />
+      </Switch>
+    </div>
+  )
+}
 
 App.propTypes = {
   location: PropTypes.shape({
@@ -84,4 +94,4 @@ App.propTypes = {
   }).isRequired,
 }
 
-export default App
+export default connect(({ user }) => ({ user }))(App)

f1648cc879144dc552a4cec5440fcd725ddcef3b
Promise Tochi <Promhize@gmail.com>
2018-03-01 14:37:18 +0100

    removed redux devtools from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,19 @@ import persistState from 'redux-localstorage'
 import rootReducer from './reducers/rootReducer'
 import network from 'services/network'
 
-const enhancer = compose(
-  applyMiddleware(thunk.withExtraArgument({ network })),
-  persistState(['auth', 'user']),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const dev = window.location.href.indexOf('localhost') > -1
+
+const enhancer = dev
+  ? compose(
+      applyMiddleware(thunk.withExtraArgument({ network })),
+      persistState(['auth', 'user']),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  : compose(
+      applyMiddleware(thunk.withExtraArgument({ network })),
+      persistState(['auth', 'user'])
+    )
 
 const initialState = undefined
 const store = createStore(rootReducer, initialState, enhancer)

bb100a58cf23b9f7a679b177adff7b4f4415133b
Promise Tochi <Promhize@gmail.com>
2018-03-01 09:22:00 +0100

    Removed confusing capitalized styling from inputs

--- src/components/form/StyledInput.js
+++ src/components/form/StyledInput.js
@@ -4,7 +4,7 @@ const StyledInput = props => (
   <fieldset>
     <input
       {...props}
-      className="w-full p-4 py-3 roboto text-base tracking-wide capitalize border border-grey-light border-solid rounded-sm "
+      className="w-full p-4 py-3 roboto text-base tracking-wide border border-grey-light border-solid rounded-sm "
     />
   </fieldset>
 )

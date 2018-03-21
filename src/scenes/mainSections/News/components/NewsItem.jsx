69f6503e92404406d5fd1f77d441438c3417dd0c
Promise Tochi <Promhize@gmail.com>
2018-03-21 08:40:51 +0100

    Initial updates for new section

--- src/scenes/mainSections/News/components/NewsItem.jsx
+++ src/scenes/mainSections/News/components/NewsItem.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NewsItem = ({ news: { author, body, createdAt, id, image, title } }) => {
   return (
-    <li className="lg:w-full lg:pl-4 mb-6">
+    <li className="lg:w-1/2 lg:pl-4 mb-6">
       <div className=" bg-white lg:lt-shadow">
         <figure
           style={
@@ -13,9 +13,7 @@ const NewsItem = ({ news: { author, body, createdAt, id, image, title } }) => {
           }
           className=""
         >
-          <div className="">
-            <img src={image} alt={title} />
-          </div>
+          <div className="">{!!image && <img src={image} alt={title} />}</div>
         </figure>
         <div className="flex-grow p-8 py-6">
           <h3 className="mb-2 text-3xl hind">{title}</h3>
